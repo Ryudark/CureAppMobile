a554305dbd6a993b91670a9d96dafa5078116513
JJ <steinu037@gmail.com>
2022-06-17 19:55:51 -0500

    estados

--- src/components/posteo/Post.jsx
+++ src/components/posteo/Post.jsx
@@ -23,18 +23,18 @@ export default function Post() {
     // const [fecha2, setFecha2] = useState(new Date())
     // const [fecha3, setFecha3] = useState(new Date())
     // const [fecha4, setFecha4] = useState(new Date())
-    const [mode, setMode]= useState('date')
-    const [show, setShow]= useState(false)
+    const [mode, setMode] = useState('date')
+    const [show, setShow] = useState(false)
     // const [text, setText]= useState({
     //     fechaIni:"",
     //     fechaFin:"",
     //     horaIni:"",
     //     horaFin:""
     // })
-    const [text, setText]= useState()
-    const [text2, setText2]= useState()
-    const [text3, setText3]= useState()
-    const [text4, setText4]= useState()
+    const [text, setText] = useState()
+    const [text2, setText2] = useState()
+    const [text3, setText3] = useState()
+    const [text4, setText4] = useState()
 
     const country = useSelector((state) => state.country);
     const region = useSelector((state) => state.region);
@@ -42,94 +42,98 @@ export default function Post() {
     const dispatch = useDispatch();
 
     const [user, setUser] = useState({
-        date_post:"",
-        hour_post:"",
-        date_ini:"",
-        date_fin:"",
-        needs:"",
-        locationReference:"",
-        contact_phone:"",
-        id_users:"",
-        specialtyPatient:"",
-        state:"",
-        city:"",
-        country:"",
-        agePatient:"",
-        namePatient:"",
-        availableTime_0:"",
-        availableTime_1:""
+        date_post: "",
+        hour_post: "",
+        date_ini: "",
+        date_fin: "",
+        needs: "",
+        locationReference: "",
+        contact_phone: "",
+        id_users: "",
+        specialtyPatient: "",
+        state: "",
+        city: "",
+        country: "",
+        agePatient: "",
+        namePatient: "",
+        availableTime_0: "",
+        availableTime_1: ""
     })
 
-    function changeDate(){
-        const date= new Date()
-        const actualDate = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()
+    function changeDate() {
+        const date = new Date()
+        const actualDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
         const actualHour = date.toLocaleTimeString()
-        setUser({...user, date_post: actualDate, hour_post:actualHour })
+        setUser({ ...user, date_post: actualDate, hour_post: actualHour })
     }
 
-    function changeDateIni(event, selectDate){
-        const currentDate = selectDate || fecha
-        console.log(currentDate)
-        setFecha(currentDate)
-        const date= new Date(currentDate)
-        const actualDate = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()
+    function changeDateIni(event, selectedDate) {
+        console.log(selectedDate)
+        const currentDate = selectedDate || fecha
+        // setFecha(currentDate)
+        const date = new Date(currentDate)
+        const actualDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
         setText(actualDate)
-        setUser({...user, date_ini: text})
+        setUser(prev => ({ ...prev, date_ini: actualDate }))
         setShow(false)
     }
 
-    function changeDateFin(event, selectDate){
-        const currentDate = selectDate || fecha
+    function changeDateFin(event, selectedDate) {
+        const currentDate = selectedDate || fecha
         console.log(currentDate)
-        setFecha(currentDate)
-        const date= new Date(currentDate)
-        const actualDate = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()
-        console.log(actualDate)
+        // setFecha(currentDate)
+        const date = new Date(currentDate)
+        const actualDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
+        // console.log(actualDate)
+        // setUser({ ...user, date_fin: actualDate })
+        setUser(prev => ({ ...prev, date_fin: actualDate }))
         setText2(actualDate)
         setShow(false)
     }
 
-    function changeHourIni(event, selectDate){
+    function changeHourIni(event, selectDate) {
         const currentDate = selectDate || fecha
         console.log(currentDate)
-        setFecha(currentDate)
-        const date= new Date(currentDate)
-        const actualHour = date.getHours()+":"+date.getMinutes()
+        // setFecha(currentDate)
+        const date = new Date(currentDate)
+        const actualHour = date.getHours() + ":" + date.getMinutes()
         console.log(actualHour)
-        setText({...text, horaIni:actualHour})
+        setText({ ...text, horaIni: actualHour })
+        setUser(prev => ({ ...prev, availableTime_0: actualDate }))
         setShow(false)
     }
 
-    function changeHourFin(event, selectDate){
+    function changeHourFin(event, selectDate) {
         const currentDate = selectDate || fecha
         console.log(currentDate)
-        setFecha(currentDate)
-        const date= new Date(currentDate)
-        const actualHour = date.getHours()+":"+date.getMinutes()
+        // setFecha(currentDate)
+        const date = new Date(currentDate)
+        const actualHour = date.getHours() + ":" + date.getMinutes()
         console.log(actualHour)
-        setText({...text, horaIni:actualHour})
+        // setText({ ...text, availableTime_1: actualHour })
+        setUser(prev => ({ ...prev, availableTime_1: actualDate }))
         setShow(false)
     }
     console.log(text)
-    
-    function showMode(currentMode){
+
+    function showMode(currentMode) {
         setShow(true)
         setMode(currentMode)
     }
 
-    function changeState(state){
-        setUser({...user, state})
+    function changeState(state) {
+        setUser({ ...user, state })
     }
 
-    function changeCity(city){
-        setUser({...user, city})
+    function changeCity(city) {
+        setUser({ ...user, city })
     }
 
-    function changeCountry(country){
-        setUser({...user, country})
+    function changeCountry(country) {
+        setUser({ ...user, country })
     }
 
-    function onSubmit(){
+    function onSubmit() {
         changeDate()
         console.log(user)
     }
@@ -137,157 +141,161 @@ export default function Post() {
         dispatch(getCountry());
     }, [dispatch]);
     return (
-    <ScrollView style={styles.container} showsVerticalScrollIndicator={false} >
+        <ScrollView style={styles.container} showsVerticalScrollIndicator={false} >
 
-        <View>
-            <Text>Fecha inicio: {text}</Text>
             <View>
-                <TouchableHighlight onPress={()=>showMode('date')}>
-                    <Text style={styles.textB}>Escoja Fecha Inicio</Text>
-                </TouchableHighlight>
+                <Text>Fecha inicio: {user.date_ini}</Text>
+                <View>
+                    <TouchableHighlight onPress={() => showMode('date')}>
+                        <Text style={styles.textB}>Escoja Fecha Inicio</Text>
+                    </TouchableHighlight>
+                </View>
+                {show && (<DateTimePicker
+                    testID='dateTimePicker'
+                    value={fecha}
+                    mode={mode}
+                    is24Hour={true}
+                    display='default'
+                    onChange={changeDateIni}
+                />)}
+                <Text>Fecha Fin: {user.date_fin}</Text>
+                <View>
+                    <TouchableHighlight onPress={() => showMode('date')}>
+                        <Text style={styles.textB}>Escoja Fecha Fin</Text>
+                    </TouchableHighlight>
+                </View>
+                {show && (<DateTimePicker
+                    testID='dateTimePicker'
+                    value={fecha}
+                    mode={mode}
+                    is24Hour={true}
+                    display='default'
+                    onChange={changeDateFin}
+                />)}
             </View>
-            {show &&(<DateTimePicker 
-                testID='dateTimePicker'    
-                value={fecha}
-                mode={mode}
-                is24Hour={true}
-                display='default'
-                onChange={changeDateIni}
-            />)}
-            <Text>Fecha Fin: {text2}</Text>
             <View>
-                <TouchableHighlight onPress={()=>showMode('date')}>
-                    <Text style={styles.textB}>Escoja Fecha Fin</Text>
-                </TouchableHighlight>
+                <View>
+                    <Text>Hora inicio: {user.availableTime_0}</Text>
+                    <TouchableHighlight onPress={() => showMode('time')}>
+                        <Text style={styles.textB}>Escoja Hora inicio</Text>
+                    </TouchableHighlight>
+                </View>
+                {show && (<DateTimePicker
+                    testID='dateTimePicker'
+                    value={fecha}
+                    mode={mode}
+                    is24Hour={true}
+                    display='default'
+                    onChange={changeHourIni}
+                />)}
+                <View>
+                    <Text>Hora Fin: {user.availableTime_1}</Text>
+                    <TouchableHighlight onPress={() => showMode('time')}>
+                        <Text style={styles.textB}>Escoja Hora Fin</Text>
+                    </TouchableHighlight>
+                </View>
+                {show && (<DateTimePicker
+                    testID='dateTimePicker'
+                    value={fecha}
+                    mode={mode}
+                    is24Hour={true}
+                    display='default'
+                    onChange={changeHourFin}
+                />)}
             </View>
-            {show &&(<DateTimePicker 
-                testID='dateTimePicker'    
-                value={fecha}
-                mode={mode}
-                is24Hour={true}
-                display='default'
-                onChange={changeDateFin}
-            />)}
-        </View>
-        {/* <View>
-            <View>
-                <Text>Hora inicio: {text.horaIni}</Text>
-                <TouchableHighlight onPress={()=>showMode('time')}>
-                    <Text style={styles.textB}>Escoja Hora inicio</Text>
-                </TouchableHighlight>
-            </View>
-            {show &&(<DateTimePicker 
-                testID='dateTimePicker'    
-                value={fecha3}
-                mode={mode}
-                is24Hour={true}
-                display='default'
-                onChange={changeHourIni}
-            />)}
-            <View>
-                <Text>Hora Fin: {text.horaFin}</Text>
-                <TouchableHighlight onPress={()=>showMode('time')}>
-                    <Text style={styles.textB}>Escoja Hora Fin</Text>
-                </TouchableHighlight>
-            </View>
-            {show &&(<DateTimePicker 
-                testID='dateTimePicker'    
-                value={fecha4}
-                mode={mode}
-                is24Hour={true}
-                display='default'
-                onChange={changeHourFin}
-            />)}
-        </View> */}
 
-        <View style={styles.containerInfoSelect}>
-        <View style={styles.containerInput}>
-            <Text style={styles.text}>País</Text>
-            <RNPickerSelect
-                onValueChange={(value) => {dispatch(getRegion(country, value))
-                                            changeCountry(value)}}
-                items={country?.map((data, index) => ({
-                key: index,
-                label: data.name,
-                value: data.name,
-            }))}
-            />
-        </View>
-        <View style={styles.containerInput}>
-            <Text style={styles.text}>Estado</Text>
-            <RNPickerSelect
-            onValueChange={(value) => {dispatch(getCity(region, value))
-                                                changeState(value)}}
-            items={region?.map((data, index) => ({
-                key: index,
-                label: data.region,
-                value: data.region,
-            }))}
-            />
-        </View>
-        <View style={styles.containerInput}>
-            <Text style={styles.text}>ciudad</Text>
-            <RNPickerSelect
-            onValueChange={(value) => {changeCity(value)}}
-            items={city?.map((data, index) => ({
-                key: index,
-                label: data.city,
-                value: data.city,
-            }))}
-            />
-        </View>
-        </View>
+            <View style={styles.containerInfoSelect}>
+                <View style={styles.containerInput}>
+                    <Text style={styles.text}>País</Text>
+                    <RNPickerSelect
+                        onValueChange={(value) => {
+                            dispatch(getRegion(country, value))
+                            changeCountry(value)
+                        }}
+                        items={country?.map((data, index) => ({
+                            key: index,
+                            label: data.name,
+                            value: data.name,
+                        }))}
+                    />
+                </View>
+                <View style={styles.containerInput}>
+                    <Text style={styles.text}>Estado</Text>
+                    <RNPickerSelect
+                        onValueChange={(value) => {
+                            dispatch(getCity(region, value))
+                            changeState(value)
+                        }}
+                        items={region?.map((data, index) => ({
+                            key: index,
+                            label: data.region,
+                            value: data.region,
+                        }))}
+                    />
+                </View>
+                <View style={styles.containerInput}>
+                    <Text style={styles.text}>ciudad</Text>
+                    <RNPickerSelect
+                        onValueChange={(value) => { changeCity(value) }}
+                        items={city?.map((data, index) => ({
+                            key: index,
+                            label: data.city,
+                            value: data.city,
+                        }))}
+                    />
+                </View>
+            </View>
 
-        <TouchableHighlight onPress={onSubmit} style={styles.butonContainer}>
-            <Text style={styles.textB}>Crear</Text>
-        </TouchableHighlight>
-    </ScrollView>
+            <TouchableHighlight onPress={onSubmit} style={styles.butonContainer}>
+                <Text style={styles.textB}>Crear</Text>
+            </TouchableHighlight>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop:20,
+        paddingTop: 20,
         paddingHorizontal: 5,
         paddingLeft: 15,
         paddingRight: 15,
     },
     input: {
-    marginHorizontal: 20,
-    borderBottomWidth: 1,
-    padding: 10,
-    borderBottomColor: "#24b8b8",
-    width: 300,
+        marginHorizontal: 20,
+        borderBottomWidth: 1,
+        padding: 10,
+        borderBottomColor: "#24b8b8",
+        width: 300,
     },
     textArea: {
-    height: 60,
+        height: 60,
     },
     containerInput: {
-    marginTop: 20,
+        marginTop: 20,
     },
 
     butonContainer: {
-    marginTop: 30,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 10,
-    paddingHorizontal: 50,
-    borderRadius: 25,
-    elevation: 3,
-    backgroundColor: "#24b8b8",
-    alignSelf: "stretch",
-    marginBottom: 30,
+        marginTop: 30,
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 10,
+        paddingHorizontal: 50,
+        borderRadius: 25,
+        elevation: 3,
+        backgroundColor: "#24b8b8",
+        alignSelf: "stretch",
+        marginBottom: 30,
     },
     text: {
-    color: "#1d3454",
-    fontSize: 18,
-    marginLeft: 15,
-    textShadowColor: "#7a7979",
+        color: "#1d3454",
+        fontSize: 18,
+        marginLeft: 15,
+        textShadowColor: "#7a7979",
     },
     textB: {
-    color: "#1d3454",
-    fontSize: 18,
+        color: "#1d3454",
+        fontSize: 18,
 
-    textShadowColor: "#7a7979",
+        textShadowColor: "#7a7979",
     },
 });
